36f3e16828f6a2c70beee2a27a6aeee7f31a3026
Wai Yan Hein <wai@cyber-duck.co.uk>
2020-02-24 10:58:08 +0000

    create the custom config the position the elements

--- assets/js/block-page.js
+++ assets/js/block-page.js
@@ -6,31 +6,4 @@
             }
         });
     });
-
-    var $autocompleteHtml = ''
-
-    function readjustBlockpageAutoComplateSearchboxPosition()
-    {
-        if ($('#Root_ContentBlocks') == null || $('#Root_ContentBlocks') == undefined) {
-            return
-        }
-
-        if ($autocompleteHtml == '') {
-            $autocompleteHtml = "<div class='add-existing-autocompleter'>" + $('#Root_ContentBlocks .add-existing-autocompleter').html() + "</div>";
-        }
-        var $existingToolbarRightHtml = $("#Root_ContentBlocks .ss-gridfield-buttonrow").find('.pull-xs-right').html()
-        $("#Root_ContentBlocks .ss-gridfield-buttonrow").find('.pull-xs-right').html('')
-        $('#Root_ContentBlocks .add-existing-autocompleter').html('')
-        $("#Root_ContentBlocks .ss-gridfield-buttonrow").find('.pull-xs-right').html($autocompleteHtml + $existingToolbarRightHtml)
-    }
-
-    $('body').on('click', '.grid-field__filter-open', function () {
-        setTimeout(readjustBlockpageAutoComplateSearchboxPosition, 2000)
-    })
-
-    $('body').on('click', '.search-box__cancel', function () {
-        setTimeout(readjustBlockpageAutoComplateSearchboxPosition, 2000)
-    })
-
-    readjustBlockpageAutoComplateSearchboxPosition()
 })(jQuery);
--- src/Extension/BlockPageExtension.php
+++ src/Extension/BlockPageExtension.php
@@ -2,12 +2,21 @@
 
 namespace CyberDuck\BlockPage\Extension;
 
-use CyberDuck\BlockPage\Action\GridFieldVersionedContentBlockItemRequest;
 use CyberDuck\BlockPage\Model\ContentBlock;
 use CyberDuck\BlockPage\Model\CustomGridFieldAddExistingAutocompleter;
 use SilverStripe\Control\Controller;
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\GridField\GridField_ActionMenu;
 use SilverStripe\Forms\GridField\GridFieldAddExistingAutocompleter;
+use SilverStripe\Forms\GridField\GridFieldAddNewButton;
+use SilverStripe\Forms\GridField\GridFieldButtonRow;
+use SilverStripe\Forms\GridField\GridFieldConfig;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
+use SilverStripe\Forms\GridField\GridFieldEditButton;
+use SilverStripe\Forms\GridField\GridFieldFilterHeader;
+use SilverStripe\Forms\GridField\GridFieldSortableHeader;
+use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
 use SilverStripe\Forms\LiteralField;
 use SilverStripe\Forms\GridField\GridField;
 use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
@@ -39,24 +48,31 @@ class BlockPageExtension extends DataExtension
     public function updateCMSFields(FieldList $fields)
     {
         if ($this->owner->ID > 0) {
-            $editor = GridFieldConfig_RelationEditor::create();
-            $grid = new GridField('ContentBlocks', 'Content Blocks', $this->owner->ContentBlocks(), $editor);
-            $grid->getConfig()
-                ->removeComponentsByType(GridFieldPageCount::class)
-                ->removeComponentsByType(GridFieldPaginator::class)
-                ->removeComponentsByType(GridFieldAddExistingAutocompleter::class)
-                ->addComponent(new CustomGridFieldAddExistingAutocompleter())
-                ->addComponent(new GridFieldOrderableRows('SortBlock'))
-                ->getComponentByType(GridFieldDetailForm::class)
-                ->setItemRequestClass(GridFieldVersionedContentBlockItemRequest::class);
+            $contentBlocksFieldConfig = GridFieldConfig::create();
+            $contentBlocksFieldConfig->addComponent(new GridFieldButtonRow('before'));
+            $contentBlocksFieldConfig->addComponent(new GridFieldAddNewButton('buttons-before-left'));
+            $contentBlocksFieldConfig->addComponent(new CustomGridFieldAddExistingAutocompleter('buttons-before-right'));
+            $contentBlocksFieldConfig->addComponent(new GridFieldToolbarHeader());
+            $contentBlocksFieldConfig->addComponent(new GridFieldSortableHeader());
+            $contentBlocksFieldConfig->addComponent(new GridFieldFilterHeader());
+            $contentBlocksFieldConfig->addComponent(new GridFieldDataColumns());
+            $contentBlocksFieldConfig->addComponent(new GridFieldEditButton());
+            $contentBlocksFieldConfig->addComponent(new GridFieldDeleteAction(true));
+            $contentBlocksFieldConfig->addComponent(new GridField_ActionMenu());
+            $contentBlocksFieldConfig->addComponent(new GridFieldDetailForm());
 
-            $detail = $grid->getConfig()->getComponentByType(GridFieldDetailForm::class);
+            $contentBlocksField = GridField::create(
+                'ContentBlocks',
+                'Content Blocks',
+                $this->owner->ContentBlocks(),
+                $contentBlocksFieldConfig
+            );
 
             $session = Controller::curr()->getRequest()->getSession();
             $session->set('BlockRelationID', $this->owner->ID);
             $session->set('BlockRelationClass', $this->owner->ClassName);
 
-            $fields->addFieldToTab('Root.ContentBlocks', $grid);
+            $fields->addFieldToTab('Root.ContentBlocks', $contentBlocksField);
         } else {
             $fields->addFieldToTab('Root.ContentBlocks', LiteralField::create(false, 'Please save this block to start adding items<br><br>'));
         }
